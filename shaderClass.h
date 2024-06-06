a248d536c6b311dfad100691bf13940148397637
CamiloVS05 <villaloboscamilo571@gmail.com>
2024-06-06 00:34:33 -0600

    añadi archivos cpp al proyecto
    uwu

--- shaderClass.h
+++ shaderClass.h
@@ -1,7 +1,11 @@
 #ifndef SHADER_CLASS_H
 #define SHADER_CLASS_H
 #include<glad/glad.h> 
-#include<iostream> #include<fstream> #include<sstream> #include<iostream> #include<cerrno>
+#include<iostream>
+#include<fstream> 
+#include<sstream>
+#include<iostream> 
+#include<cerrno>
 
 std::string get_file_contents(const char* filename); class Shader {
 public:
