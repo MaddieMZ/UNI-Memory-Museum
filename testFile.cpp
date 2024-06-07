10ec6fbe0c45e4b15b3986f9512d8a1234281975
Sergio200423 <sergiodanielxd2004@gmail.com>
2024-06-06 23:03:27 -0600

    Modificacion al default.vert y modificacion al testFile

--- testFile.cpp
+++ testFile.cpp
@@ -2,29 +2,35 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<stb_image.h>
-
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include"Texture.h"
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
+const unsigned int width = 800;
+const unsigned int height = 800;
 //Coordenadas de Vertices
 GLfloat vertices[] =
 {
-	// COORDENADAS COLORES COORDENADAS PARA LA TEXTURE
-	- 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	// Lower left corner
-	-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-	// Upper left corner
-	0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-	//Upper right corner
-	0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	// Inner left
+	// COORDENADAS COLORES COORDENADASPARA LA TEXTURE
+	- 0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f,
+	0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f,
+	0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f,
+	0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f
 };
 // Indices for vertices order
 GLuint indices[] =
 {
-0, 2, 1, // Upper triangle
-0, 3, 2, // Lower triangle
+0, 1, 2,
+0, 2, 3,
+0, 1, 4,
+1, 2, 4,
+2, 3, 4,
+3, 0, 4
 };
 int main()
 {
@@ -37,8 +43,8 @@ int main()
 	// Tell GLFW we are using the CORE profile
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// Create a GLFWwindow object of 800 by 800 pixels
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Graficas de Computadoras de manera Estructurada", NULL, NULL);
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(width, height, "Bienvenidos al 3D usando OpenGL", NULL, NULL);
 		// Error check if the window fails to create
 		if (window == NULL)
 		{
@@ -52,7 +58,7 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
 	// Generates Vertex Array Object and binds it
@@ -77,7 +83,10 @@ int main()
 	//Texture
 	int widthImg, heightImg, numColCh;
 	stbi_set_flip_vertically_on_load(true);
-	unsigned char* bytes = stbi_load("yoshii.png", &widthImg, &heightImg, &numColCh, 0);
+	unsigned char* bytes = stbi_load("yoshii.png", &widthImg, &heightImg,
+		&numColCh, 0);
+	//Texture dog("dog.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	//dog.texUnit(shaderProgram, "tex0", 0);
 	GLuint texture;
 	glGenTextures(1, &texture);
 	glActiveTexture(GL_TEXTURE0);
@@ -87,23 +96,55 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	//Para ubicar la textura
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
-	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, bytes);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,GL_UNSIGNED_BYTE, data);
 	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(bytes);
+	//stbi_image_free(bytes);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
 	shaderProgram.Activate();
 	glUniform1i(tex0Uni, 0);
+	//Variables para controlar la rotacion
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+	glEnable(GL_DEPTH_TEST);
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+		//Control de la Rotation de la Piramide con la textura incorporada pormedio de la funcion del tiempo.
+			double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1 / 60)
+		{
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+		//Inicializacion de las matrices para ser utilizadas
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+		//Asignamos diferentes transformaciones a las matrices que tenemospreviamente declaradas
+			//Para controlar la rotacion de la piramide
+			model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f,
+				0.0f));
+		//Para establecer la matriz de traslacion
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		//Para establecer la proyeccion en perspectiva
+		proj = glm::perspective(glm::radians(45.05f), (float)(width / height),
+			0.1f, 100.0f);//Posicion de la Camara
+		//Entradas de las matrices para ser usadas en el Vertex Shader
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 		// Assigns a value to the uniform
 		shaderProgram.Activate();
 		//Assigns a value to the uniform
@@ -111,8 +152,9 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
-		// Draw primitives, number of indices, datatype of indices, index of indices
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// Draw primitives, number of indices, datatype of indices, index ofindices
+			glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT,
+				0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
